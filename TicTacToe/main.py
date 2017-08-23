9823099d451124c04deee24504225930a8b5b7ed
Rickard Östlund <ricost@ed3354-07.ed3354.studat.rh69.ii.2.hb.s.cdal.chalmers.se>
2017-08-23 09:36:21 +0200

    broke everything

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,15 +1,12 @@
 __author__ = 'chrarvi'
 
 grid = []
+coords = [[" " for x in range(3)] for y in range(3)]
 
 def genGrid():
     tempStr = ""
     for i in range(8):
         grid.append('#')
-
-
-
-
 
     #[0|0|#]
     #[-----]
@@ -18,16 +15,59 @@ def genGrid():
     #[0|0|0]
 
 def init():
-    print """
-             [%s||?]
-             -------
-             [?|?|?]
-             -------
-             [?|?|?]
-             -------
-        """
+     grid = (coords[0][0],
+             coords[1][0],
+             coords[2][0],
+             coords[0][1],
+             coords[1][1],
+             coords[1][2],
+             coords[2][0],
+             coords[2][1],
+             coords[2][2])
 
+     board =  """
+                  0 1 2 
+              0  [{0}|{1}|{2}]
+                 -------
+              1  [{3}|{4}|{5}]
+                 -------
+              2  [{6}|{7}|{8}]
+                 -------
+               """.format(grid[0])
+                      
+                      
+                      
+                      
+                      
+     print board
+def hasWon():
+     winConditions = (
+                         [[0,0],[1,0],[2,0]],
+                         [[0,1],[1,1],[2,1]],
+                         [[0,2],[1,2],[2,2]],
 
+                         [[0,0],[0,1],[0,2]],
+                         [[1,0],[1,1],[1,2]],
+                         [[2,0],[2,1],[2,2]],
+
+                         [[0,0],[1,1],[2,2]],
+                         [[2,0],[1,1],[0,2]]
+                     )
+    # for cond in winConditions:
+     #     if coords[cond[0][0]][conds[0,1]]
+
+     victor = None
+     return False
+def interact(player = 1):
+     nextPlayer = None
+     if hasWon():
+          return
+     elif player == 1:
+          nextPlayer = 2
+     else:
+          nextPlayer = 1
+
+     interact(nextPlayer)
 
 if __name__ == "__main__":
     genGrid()
@@ -37,6 +77,6 @@ if __name__ == "__main__":
     player1 = ()
     player2 = ()
     i = 0
-    for grid
-        if grid == "0"
-            player
+    #for grid
+     #   if grid == "0"
+      #      player
